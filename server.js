f41cc4ec1208d4d6ea1cdad12d7b40fffc8bd2fc
Himanshu Singh <singhh208@gmail.com>
2022-12-08 20:01:09 +0530

    Before React

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+const {MongoClient}=require("mongodb")
 const express=require('express')
+let db
 
 const app=express()
+app.set("view engine","ejs")
+app.set("views","./views")
 
-app.get('/',(req,res)=>{
-    res.send('home page')
+app.get('/',async (req,res)=>{
+    const allAnimals=await db.collection("animals").find().toArray()
+    
+    res.render("home",{allAnimals})
 })
 
 app.get('/admin',(req,res)=>{
-    res.end('admin page')
+    res.send('admin page')
 })
-app.listen(3000)
+
+async function start(){
+    const client = new MongoClient("mongodb://root:root@localhost:27017/AmazingMernApp?&authSource=admin")
+    await client.connect()
+    db=client.db()
+    app.listen(3000)
+}
+start()
